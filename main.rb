e23acd525776748d25d7dba94e8a7ef76bd290ac
José Jonathan Emmanuel <jonthan-emmanuel.jose@epitech.eu>
2020-06-18 15:55:45 +0200

    I can detect files type and count them

--- main.rb
+++ main.rb
@@ -1,1 +1,26 @@
-puts "Hello to this new project"
+$lang_files = {c: 0, cpp: 0}
+
+def is_aDir?(directory)
+    File.directory?(directory)
+end
+
+def check_src_files_on(location)
+    Dir.chdir location
+    res = Dir.children(".").to_ary
+    for i in 0...res.length
+        if is_aDir?(res[i])
+            check_src_files_on(res[i])
+        end
+        if res[i].end_with?(".c")
+            $lang_files[:c] += 1
+        end
+    end
+    Dir.chdir "../"
+end
+
+def main()
+    check_src_files_on(".")
+    puts $lang_files
+end
+
+main()
